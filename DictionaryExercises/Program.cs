f6ed4c58965e20cf1cb8b53f9f0e61782013e843
Melinda <codegirl78@gmail.com>
2021-07-21 13:24:07 -0500

    Made a program which takes in students names and ID numbers

--- DictionaryExercises/Program.cs
+++ DictionaryExercises/Program.cs
@@ -1,12 +1,40 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace DictionaryExercises
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var students = new Dictionary<int, string>();
+            string newStudent;
+
+            Console.WriteLine("Enter your students (or ENTER to finish):");
+            do
+            {
+                Console.WriteLine("name: ");
+                newStudent = Console.ReadLine();
+
+                if (newStudent != "")
+                {
+                    // Get the student's id
+                    Console.Write("id: ");
+                    var newId = int.Parse(Console.ReadLine());
+
+                    students.Add(newId, newStudent);
+                }
+            }
+            while (newStudent != "");
+
+            // Print student roster
+            Console.WriteLine("\nClass roster:");
+            foreach (KeyValuePair<int, string> student in students)
+            {
+                Console.WriteLine(student.Key + " (" + student.Value + ")");
+            }
+
+            Console.ReadLine();
         }
     }
 }

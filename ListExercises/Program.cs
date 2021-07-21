aa081ac93d320902661abfd3e5992f2a2bd06308
Melinda <codegirl78@gmail.com>
2021-07-21 12:56:30 -0500

    Wrote statis method to find the sum of all of the even numbers in a list and within Main created a list  with 10 integers and called the method on the list

--- ListExercises/Program.cs
+++ ListExercises/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ListExercises
 {
@@ -6,7 +7,20 @@ namespace ListExercises
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Lists!");
+            var numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            Console.WriteLine(EvenTotal(numbers));
+        }
+        private static int EvenTotal(List<int> numbers)
+        {
+            var total = 0;
+
+            foreach (var number in numbers)
+            {
+                if (number % 2 == 0) total += number;
+            }
+
+            return total;
+
         }
     }
 }

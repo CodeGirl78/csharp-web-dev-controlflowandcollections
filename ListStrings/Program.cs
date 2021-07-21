ee4d0c8c5ae060de06bd069a1b07bf1c4c859488
Melinda <codegirl78@gmail.com>
2021-07-21 13:14:41 -0500

    Wrote a static method to print each world in a list that has exactly 5 letters

---
+++ ListStrings/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var words = new List<string> { "Italy", "Canada", "Egypt", "US", "Madagascar", "Spain" };
+            PrintFiveLetterWords(words);
+        }
+        private static void PrintFiveLetterWords(List<string> words)
+        {
+            foreach (var word in words)
+            {
+                if (word.Length == 5) Console.WriteLine(word);
+            }
+
+        }
+    }
+}
